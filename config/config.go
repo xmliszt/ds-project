a105dd10b698e575c2c32d9fe525a661518c8be0
xmliszt <xmliszt@gmail.com>
2021-03-07 16:05:10 +0800

    feat: function to load config

--- config/config.go
+++ config/config.go
@@ -1,15 +1,48 @@
 package config
 
-type GlobalConfig struct {
-	NodeNumber int
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	ConfigServer `yaml:"Server"`
+	ConfigNode `yaml:"Node"`
 }
 
-var DefaultConfig = GlobalConfig{
-	NodeNumber: 5,
+type ConfigServer struct {
+	Host string `yaml:"Host"`
+	Port int `yaml:"Port"`
 }
 
-func LoadConfig() *GlobalConfig {
-	cfg := &GlobalConfig{}
-	*cfg = DefaultConfig
-	return cfg
+type ConfigNode struct {
+	Number int `yaml:"Number"`
 }
+
+var configPath = "../config.yaml"
+
+// It loads the config from YAML file and return the config object
+func LoadConfig() (*Config, error) {
+	cfg := &Config{}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	
+	d := yaml.NewDecoder(file)
+
+	if err := d.Decode(&cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+// Custom compare function for nested struct
+func (config *Config) IsEqual(xConfig *Config) bool {
+	return config.ConfigServer == xConfig.ConfigServer && config.ConfigNode == xConfig.ConfigNode 
+}
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module "github.com/xmliszt/e-safe"
+module github.com/xmliszt/e-safe
 
 go 1.15
+
+require gopkg.in/yaml.v2 v2.4.0
