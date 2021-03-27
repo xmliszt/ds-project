e14c92ebc280cb63a5bba6c5fbbb9781ffa4bf44
xmliszt <xmliszt@gmail.com>
2021-03-27 14:33:58 +0800

    fix: linting

--- pkg/rpc/signal.go
+++ pkg/rpc/signal.go
@@ -1,6 +1,8 @@
 package rpc
 
 import (
+	"log"
+
 	"github.com/xmliszt/e-safe/pkg/data"
 )
 
@@ -11,7 +13,10 @@ type Signal interface {
 // SendSignal used for sending request to target Node
 func (n *Node) SendSignal(pid int, data *data.Data) {
 	defer func() {
-		recover()
+		r := recover()
+		if r != nil {
+			log.Println("Signal recovery: ", r)
+		}
 	}()
 	// fmt.Printf("[%d] -> [%d]: %v\n", n.Pid, pid, data)
 	sendingChannel := n.RpcMap[pid]
