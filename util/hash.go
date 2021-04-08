e7d32aad5d98d056a48fbbb9d3215dd09fe17c2b
xmliszt <xmliszt@gmail.com>
2021-04-08 14:50:24 +0800

    feat: multiple hashing recursively

--- util/hash.go
+++ util/hash.go
@@ -4,16 +4,29 @@ import (
 	"hash/fnv"
 	"log"
 	"strconv"
+
+	"github.com/xmliszt/e-safe/config"
 )
 
 func GetHash(s string) (uint32, error) {
-	h := fnv.New32a()
-	_, err := h.Write([]byte(s))
+	config, err := config.GetConfig()
 	if err != nil {
 		return 0, err
 	}
-	return h.Sum32(), nil
+
+	var un uint32
+	for i := 0; i < config.NumberOfHashing; i++ {
+		h := fnv.New32a()
+		_, err := h.Write([]byte(s))
+		if err != nil {
+			return 0, err
+		}
+		un = h.Sum32()
+		s = strconv.Itoa(int(un))
+	}
+	return un, nil
 }
+
 func StringHashToUint(hashedValue string) uint32 {
 	iHashedValue, intConvertError := strconv.Atoi(hashedValue)
 	if intConvertError != nil {
